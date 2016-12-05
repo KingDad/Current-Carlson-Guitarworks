b92027d22724d39ab4f70ff14088daf0951fbee8
KingDad <pgrcarlson@gmail.com>
2016-12-05 12:14:39 -0500

    Img Gallery
    Added an image gallery and did some general clean up.

--- js/application.js
+++ js/application.js
@@ -1,13 +1,45 @@
-$(document).ready(function(){
-	$('.list6').on('click', '.initial-button', function(e){
-		e.preventDefault();
-		$('.hidden-list').addClass('shown-list');
-		$('.initial-button').addClass('secondary-button').removeClass('initial-button').text('Hide Full List');
-		console.log ('itworked');
-	});
-	$('.list6').on('click', '.secondary-button', function(e){
-		e.preventDefault();
-		$('.hidden-list').removeClass('shown');
-		$('.secondary-button').addClass('initial-button').removeClass('secondary-button').text('Full List of Services');
-	});
-});
+function lightBox(){
+//Variable declarations
+	window.dark = document.getElementById('filter');
+	window.imgs = document.getElementsByClassName('img');
+	window.galleryImgs = document.getElementsByClassName('gallery-img');
+	window.previous = document.getElementById('previous');
+	window.next = document.getElementById('next');
+	window.index = null;
+//The lightbox begins
+//Not necessarily optimized but wanted to get a working version up first
+	for (i = 0, len = imgs.length; i < len; i++){
+		imgs[i].onclick = function(){
+			index = this.getAttribute('data-index');
+			dark.style.display = 'block';
+			galleryImgs[index].style.display = 'block';
+			previous.style.display = 'block';
+			next.style.display = 'block';
+		}
+	}
+	dark.onclick = function(){
+		dark.style.display = 'none';
+		galleryImgs[index].style.display = 'none';
+		previous.style.display = 'none';
+		next.style.display = 'none';
+	}
+//Creating a carousel effect with buttons
+	previous.onclick = function(){
+		galleryImgs[index].style.display = 'none';
+		index--;
+		if(index < 0){
+			index = 4;
+		}
+		galleryImgs[index].style.display = 'block';
+	}
+	next.onclick = function(){
+		galleryImgs[index].style.display = 'none';
+		index++;
+		if(index > 4){
+			index = 0;
+		}
+		galleryImgs[index].style.display = 'block';
+	}
+	return "Radical";
+}
+lightBox();
--- lightbox.js
+++
@@ -1,3 +0,0 @@
-function lightBox(){
-	var index = document.getElementById
-}
